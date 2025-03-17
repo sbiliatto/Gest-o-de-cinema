1aa9c2434686d374cf813dcb7d97cf461615709a
juliatibe <juliatiberio94@gmail.com>
2025-03-17 15:44:08 -0300

    atividadess

--- Gestão de cinema/assets/js/seguranca.js
+++ Gestão de cinema/assets/js/seguranca.js
@@ -2,9 +2,11 @@ let token = localStorage.getItem("token")
 
 // Verifica se NÃO POSSUI um token
 if(token === undefined || token === null) {
-    alert("Essa Página é restrita");
-    
-
-    // Manda para a pagina de login
-    window.location.href = "login.html"
+    Swal.fire({
+        title: "Deu Errado!",
+        text: "Essa Página é restrita!",
+        icon: "error"
+    }).then(() => {
+        window.location.href = "login.html";
+    });
 }
