c8b791bce1c02900246692b3bafa030f617892f0
ygor <167575437+evelynM07@users.noreply.github.com>
2025-03-17 14:56:15 -0300

    update segurança

--- Gestão de cinema/assets/js/seguranca.js
+++ Gestão de cinema/assets/js/seguranca.js
@@ -3,6 +3,7 @@ let token = localStorage.getItem("token")
 // Verifica se NÃO POSSUI um token
 if(token === undefined || token === null) {
     alert("Essa Página é restrita");
+    
 
     // Manda para a pagina de login
     window.location.href = "login.html"
