3d4097102f1c207c83f0828a5b5806e1c550fe85
juliatibe <juliatiberio94@gmail.com>
2025-03-18 14:47:57 -0300

    dfgvdfxg

--- Gestão de cinema/assets/js/seguranca.js
+++ Gestão de cinema/assets/js/seguranca.js
@@ -1,12 +1,12 @@
-let token = localStorage.getItem("token")
+let token = localStorage.getItem("token") // pega o token armazenado no localStorage
 
-// Verifica se NÃO POSSUI um token
-if(token === undefined || token === null) {
-    Swal.fire({
+
+if(token === undefined || token === null) { //verifica se o token esta ausente, se o usuario não ta autenticado
+    Swal.fire({ //exibe um alerta de que a página é restrita
         title: "Erro!",
         text: "Essa Página é restrita!",
         icon: "error"
     }).then(() => {
-        window.location.href = "login.html";
+        window.location.href = "login.html"; //volta para a pagina de login
     });
 }
